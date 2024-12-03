d82b6cef1aba16c8c17a1254679862dfa50658f9
solmael <solmael13@gmail.com>
2024-12-03 16:39:27 +0300

    added brain_even script

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,2 +1,10 @@
-def is_even():
-    print('gh')
+#!/usr/bin/env python3
+from brain_games.games.even import run_game_even
+
+
+def main():
+    run_game_even()
+
+
+if __name__ == '__main__':
+    main()
