f86ec626714f80fe106a127568d249446a2e8994
solmael <solmael13@gmail.com>
2024-12-03 17:49:36 +0300

    update even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,18 @@
+import random
+from brain_games.engine import start_game
+from brain_games.const import EVEN_INSTRUCTION
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def get_num_and_answer():
+    num = random.randint(1, 100)
+    answer = 'yes' if is_even(num) else 'no'
+
+    return num, answer
+
+
+def run_game_even():
+    start_game(get_num_and_answer, EVEN_INSTRUCTION)
