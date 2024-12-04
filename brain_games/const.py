a6fd99bc7b7c3aab65eee1328fd51bad91b6349a
solmael <solmael13@gmail.com>
2024-12-04 16:31:21 +0300

    updated const.py

--- brain_games/const.py
+++ brain_games/const.py
@@ -1,4 +1,6 @@
 NUM_OF_ROUNDS = 3
 
+MATH_SIGNS = ('*', '+', '-')
 
 EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+CALC_INSTRUCTION = 'What is the result of the expression?'
