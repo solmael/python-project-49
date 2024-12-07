d416cb5eb2ed4faeb45a35cdab71d1ed8d24878d
solmael <solmael13@gmail.com>
2024-12-07 14:14:00 +0300

    edited const.py by flake8

--- brain_games/const.py
+++ brain_games/const.py
@@ -6,4 +6,5 @@ EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression?'
 GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
 PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
-PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. '\
+                    'Otherwise answer "no".'
