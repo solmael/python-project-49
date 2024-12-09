a0204df4cd8fc53267c7a870e06c9be874b84dda
solmael <solmael13@gmail.com>
2024-12-09 08:07:20 +0300

    added prime.py

---
+++ brain_games/games/prime.py
@@ -0,0 +1,24 @@
+import random
+
+from brain_games.engine import start_game
+from brain_games.const import PRIME_INSTRUCTION
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def get_num_and_answer():
+    num = random.randint(1, 100)
+    answer = 'yes' if is_prime(num) else 'no'
+
+    return num, answer
+
+
+def run_game_prime():
+    start_game(get_num_and_answer, PRIME_INSTRUCTION)
