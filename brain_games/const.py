03596c1ba5c67945b40e20d632d6392b61abf081
solmael <solmael13@gmail.com>
2024-12-16 08:56:39 +0300

    const.py updated

--- brain_games/const.py
+++ brain_games/const.py
@@ -1,7 +1,5 @@
 NUM_OF_ROUNDS = 3
 
-MATH_SIGNS = ('*', '+', '-')
-
 EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression?'
 GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
