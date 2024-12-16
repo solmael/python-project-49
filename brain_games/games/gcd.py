49a8ece658913dd729d90b63547584122c9cc732
solmael <solmael13@gmail.com>
2024-12-16 11:20:03 +0300

    gcd.py updated

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,13 +1,12 @@
-import math
-import random
-
+from math import gcd
+from random import randint
 from brain_games.const import GCD_INSTRUCTION
 from brain_games.engine import start_game
 
 
 def get_nums_and_result():
-    num1, num2 = random.randint(1, 100), random.randint(1, 100)
-    result = str(math.gcd(num1, num2))
+    num1, num2 = randint(1, 100), randint(1, 100)
+    result = str(gcd(num1, num2))
     couple_nums = f'{num1} {num2}'
 
     return couple_nums, result
