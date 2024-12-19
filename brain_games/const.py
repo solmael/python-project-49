a6ddbb9c7fd3556a566eea1b984fc3e0bc8709ad
solmael <solmael13@gmail.com>
2024-12-19 15:03:04 +0300

    deleted const.py & random_utils.py

--- brain_games/const.py
+++
@@ -1,8 +0,0 @@
-NUM_OF_ROUNDS = 3
-
-EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-CALC_INSTRUCTION = 'What is the result of the expression?'
-GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
-PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
-PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. '\
-                    'Otherwise answer "no".'
